26f85693bdb051f786e6eee44d3844ee97919dd0
Ederson rodrigo <edersonrodrigo31@gmail.com>
2021-05-16 13:03:10 -0300

    [INFRA]: create post migration

---
+++ backend/src/infra/orm/typeorm/migrations/1621179333799-Posts.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm'
+
+export class Posts1621179333799 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'posts',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid'
+          },
+          {
+            name: 'image',
+            type: 'varchar'
+          },
+          {
+            name: 'description',
+            type: 'varchar'
+          },
+          {
+            name: 'authorId',
+            type: 'varchar'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ]
+      }))
+
+    await queryRunner.createForeignKey('posts', new TableForeignKey({
+      name: 'Author',
+      columnNames: ['authorId'],
+      referencedColumnNames: ['id'],
+      referencedTableName: 'users',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    }))
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('posts')
+  }
+}
